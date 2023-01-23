5ff8bed6cf8fce12c97aedeedc78bd2864f4f220
zhijunlin7 <zlin1@cifpfbmoll.eu>
2023-01-23 18:08:19 +0100

    hecho

--- app/src/main/java/com/example/drawablesstylesandthemes/MainActivity.java
+++ app/src/main/java/com/example/drawablesstylesandthemes/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.drawablesstylesandthemes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.drawablesstylesandthemes.databinding.ActivityMainBinding;
@@ -21,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         binding.team2minus.setOnClickListener(this::decrementar);
         binding.team1plus.setOnClickListener(this::aumentar);
         binding.team2plus.setOnClickListener(this::aumentar);
+
+        if (savedInstanceState != null) {
+            punto1 = savedInstanceState.getInt("point1");
+            punto2 = savedInstanceState.getInt("point2");
+
+            //Set the score text views
+            binding.point1.setText(String.valueOf(punto1));
+            binding.point2.setText(String.valueOf(punto2));
+        }
 
     }
 
@@ -49,5 +62,39 @@ public class MainActivity extends AppCompatActivity {
                 binding.point2.setText(String.valueOf(punto2));
                 break;
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.night_mode){
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if(nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        }
+        recreate();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt("point1",punto1);
+        outState.putInt("point2",punto2);
+        super.onSaveInstanceState(outState);
     }
 }
